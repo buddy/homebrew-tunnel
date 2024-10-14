1010f6d19a2755dfd70d9a9c136307b3cbb4153e
Michal Hankus <michal83h@gmail.com>
2024-10-14 07:20:04 +0000

    New dev version 1.7.34

--- Formula/buddy-tunnel-dev.rb
+++ Formula/buddy-tunnel-dev.rb
@@ -1,9 +1,9 @@
 class BuddyTunnelDev < Formula
   desc "Buddy tunnel cli"
   homepage "https://buddy.works"
-  url "https://es.buddy.works/tunnel/dev/1.7.33/darwin-arm64.tar.gz"
-  sha256 "dcf6dd9334e21dad919714f2641bcddc1a6402459a220ce78b6926da8f262b01"
-  version "1.7.33"
+  url "https://es.buddy.works/tunnel/dev/1.7.34/darwin-arm64.tar.gz"
+  sha256 "452675a256e26a1c9e90ca5ffb69ec766ef5c8d8ecb5529dbb45e6f56f4ff817"
+  version "1.7.34"
   def install
     bin.install "buddy-tunnel"
   end
